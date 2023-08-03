e021418cb6f3cedf56398ca8e286e7b52403aa83
reisfellipe <125813498+reisfellipe@users.noreply.github.com>
2023-08-02 21:53:02 -0300

    fix

---
+++ Exercises.js/script-005.js
@@ -0,0 +1,13 @@
+var a = 30;
+var b = 50;
+var c = 20;
+
+//verifying if A is bigger than  B and B is divisible for 2
+console.log(a > b && b % 2 == 0);
+
+console.log(b >= a || b / 2 == 2);
+
+console.log(a > b && b >= 5);
+
+//verificandos se B é par ou impar
+resultado = b % 2 == 0 ? "Par" : "Impar";
---
+++ Exercises.js/script-04.js
@@ -0,0 +1,20 @@
+var n = 5;
+n *= 10; //mesma coisa que n = n * 10;
+console.log(n)
+
+var x = 10;
+x++
+
+var b = 20;
+var c = 50;
+
+console.log(x)
+console.log(n <= x || b != c);
+
+if(b <= c) {
+    console.log(`Seu numero é ${b}`);
+    console.log(`You have ${b} tasks to fix now`);
+}else {
+    console.log("Sua variavel precisa ser igual a outra")
+}
+
