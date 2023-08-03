351b3c072d13745cf81a6708001f91898ebe2e8b
reisfellipe <125813498+reisfellipe@users.noreply.github.com>
2023-08-02 22:22:37 -0300

    arrays

---
+++ Exercises.js/script-006.js
@@ -0,0 +1,7 @@
+//Formas diferentes de criar Arrays no JavaScript
+
+var primeira = new Array(550, 'Fellipe Reis', 18, '365 dias');
+
+var segunda = Array(360, 'Fellipe Reis', 14, '12 meses');
+
+var terceira = [77, 'Fellipe Reis', 1.220, '7 semanas'];
