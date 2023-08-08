063e33f604e009b5710c65d2d9dbd6020c5b7640
reisfellipe <125813498+reisfellipe@users.noreply.github.com>
2023-08-08 20:03:57 -0300

    switch

---
+++ Exercises.js/switch-018.js
@@ -0,0 +1,24 @@
+switch("Cereja"){
+    case "Laranja":
+        console.log("A laranja custam R$0.59 o quilo.");
+        break;
+    case "Maçã":
+        console.log("Maçã custam R$0.32 o quilo.");
+        break;
+    case "Manga":
+        console.log("Mangas custam R$3.00 o quilo.");
+        break;
+    case "Banana":
+        console.log("Bananas custam R$0.48 o quilo.");
+        break;
+    case "Maracujá":
+        console.log("Maracujás custam R$0.89 o quilo.");
+        break;
+    case "Cereja":
+        console.log("Cerejas custam R$2.89 o quilo.");
+        break;
+    default:
+        console.log(`Desculpe, estamos sem nenhuma.`);
+}
+
+console.log("Tem algo mais que você gostaria de levar?");
