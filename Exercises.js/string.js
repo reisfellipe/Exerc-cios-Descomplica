1180587c1d16d80818e16cd5e5179da49ca107ac
reisfellipe <125813498+reisfellipe@users.noreply.github.com>
2023-08-24 20:45:30 -0300

    learning js

---
+++ Exercises.js/string.js
@@ -0,0 +1,21 @@
+var nome = "Fellipe Reis da silva"
+var x = nome.length;
+console.log(x); 
+
+res = x > 15 ? `Seja bem vindo ${nome}` : "Por favor preencha o campo corretamente";
+
+console.log(res);
+
+var frase = "      Eu estou aprendendo JavaScript        ";
+console.log(frase.trim());
+
+var num = "1|2|3|4|5|6|7|8";
+var arr1 = num.split("|");
+console.log(arr1[4]);
+
+var num = "1|2|3|4|5|6|7|8";
+console.log(num[4]);
+
+var nomemae = "Marcia aparecida";
+var mae = nomemae.toUpperCase();
+console.log(mae)
