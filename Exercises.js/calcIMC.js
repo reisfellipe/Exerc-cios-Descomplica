6e37788eb368a2ca3070b258e6a36ec87d297f45
reisfellipe <125813498+reisfellipe@users.noreply.github.com>
2023-08-25 22:04:14 -0300

    calcIMC

---
+++ Exercises.js/calcIMC.js
@@ -0,0 +1,27 @@
+function criarPessoa(nome,sobrenome,a,p){
+    return {
+        nome,
+        sobrenome,
+        altura: a,
+        peso: p,
+        calculoImc(){
+            const indice = this.peso/(this.altura ** 2).toFixed(1)
+            if(indice < 18.5){
+                console.log("Abaixo do peso")
+            }else if(indice >= 18.5 && indice < 24.9){
+                console.log("Peso normal")
+            }else if(indice >= 25 && indice < 29.9){
+                console.log("Acima do peso")
+            }else if(indice >= 29.9 && indice < 34.9){
+                console.log("Obesidade 1")
+            }else if(indice >= 35 && indice < 39.9){
+                console.log("Obesidade 2")
+            }else if(indice >= 40){
+                console.log("Obesidade 3")
+            }
+        }
+    }
+}
+
+const p1 = criarPessoa("Fellipe", "Reis", 1.69, 58);
+p1.calculoImc();
